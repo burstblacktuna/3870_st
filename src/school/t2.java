2052d505f14cbfc8069cf88f2ed45e34ec0fb6a8
4100C023 <you@example.com>
2021-10-18 16:29:51 +0800

    a

--- src/school/t2.java
+++ src/school/t2.java
@@ -1,9 +1,23 @@
 package school;
 import java.util.Arrays;
+import java.util.Scanner;
+
 import static java.lang.System.*;
 
 public class t2 {
     public static void main(String[] args) {
+        var a = new Scanner(in);
+        var x = a.nextInt();
+        if (x > 0) {
+            out.println(x);
+        } else {
+            out.println(-x);
+        }
 
+        if (x % 2 == 0) {
+            out.println("整數");
+        } else {
+            out.println("奇數");
+        }
     }
 }
