64508082633635b65216d01228890c1b56c72946
4100C023 <you@example.com>
2021-10-25 16:28:02 +0800

    a

--- src/school/t4.java
+++ src/school/t4.java
@@ -4,7 +4,26 @@ import static java.lang.System.*;
 
 public class t4 {
     public static void main(String[] args) {
+        double s = 3500;
+        for (int d = 1; true; d++) {
+            s = s / 2;
+            out.println(s);
+            if (s < 3) {
+                out.println("天數:" + d);
+                break;
+            }
+        }
+        out.println();
+
         int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            sum = (int) (sum + Math.pow(i, 2));
+
+        }
+        out.println("1到100平方總和:" + sum);
+
+        out.println();
+
         double sum1 = 0;
         for (int i = 1; i <= 100; i++) {
             if (i % 16 == 0) {
@@ -13,11 +32,6 @@ public class t4 {
         }
         out.println();
 
-        for (int i = 1; i <= 100; i++) {
-            sum = (int) (sum+Math.pow(i,2));
-
-        }
-        out.println("1到100平方總和:"+sum);
 
         for (int i = 0; i <= 5; i++) {
             for (int w = 1; w <= i; w++) {
@@ -28,21 +42,11 @@ public class t4 {
         out.println();
 
         for (double i = 1; i <= 10; i++) {
-            sum1=sum1+1/i;
+            sum1 = sum1 + 1 / i;
 
 
         }
         out.println(sum1);
-
-        int sum2=0;
-        for (int i = 1; i <= 20; i++) {
-            sum2 =sum2+i;
-            out.println(i+"\t"+sum2);
-            if(sum2>=100){
-                out.println(">100 i="+i);
-                break;
-            }
-        }
 
     }
 }
