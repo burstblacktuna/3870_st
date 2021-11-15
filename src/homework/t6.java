6665456de2ee153ea9b12254987ccd425c7f6f6c
burstblacktuna <a38708242@gmail.com>
2021-11-15 17:17:37 +0800

    q

---
+++ src/homework/t6.java
@@ -0,0 +1,32 @@
+package homework;
+import static java.lang.System.*;
+
+public class t6 {
+    public static void main(String[] args) {
+        int max, min, count1 = 0;
+        int maxIndex = 0, minIndex = 0;
+        int i = 0;
+        int array[] = {3, 5, 1, 3, 2, 4, 1, 6, 8, 5, 4, 3, 2};
+        out.println("(a):" + array.length);
+        for (i = 0; i < array.length; i++) {
+            if (array[i] >= 3) if (array[i] <= 6) {
+                count1++;
+            }
+        }
+        out.println("(b):" + count1);
+        max = min = array[0];
+        for (i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+                maxIndex = i + 1;
+            }
+            if (array[i] < min) {
+                min = array[i];
+                minIndex = i + 1;
+            }
+        }
+        out.println(maxIndex);
+        out.println(minIndex);
+    }
+}
+
