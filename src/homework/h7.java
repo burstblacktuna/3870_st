071c882f569d803c4b1aa93b07815c79e3dcc952
burstblacktuna <a38708242@gmail.com>
2021-11-22 16:51:08 +0800

    q

--- src/homework/h7.java
+++ src/homework/h7.java
@@ -5,7 +5,7 @@ import static java.lang.System.*;
 public class h7 {
     public static void main(String[] args) {
         int[] A = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int sum = 0, average = 0;
+        int sum = 0, average;
         for (int i : A) {
             sum += i;
         }
