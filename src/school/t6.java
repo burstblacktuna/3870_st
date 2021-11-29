f43b13bff28a51e8997c747fe43926458bfb809a
burstblacktuna <59651678+burstblacktuna@users.noreply.github.com>
2021-11-29 18:10:42 +0800

    a

--- src/school/t6.java
+++ src/school/t6.java
@@ -1,19 +1,12 @@
 package school;
+
 import static java.lang.System.*;
+
 public class t6 {
     public static void main(String[] args) {
-
-
-        for(int i=1;i<=5;i++) {
-            for(int j=1;j<=i;j++){
-
-                out.print(j);
-
-
-            }
-            out.println();
-        }
-
+        boolean a,b,c,d;
+        a='a'<28;
+        out.println(a);
 
 
     }
