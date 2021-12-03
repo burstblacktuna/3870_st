6dedce15ee30917b47d647cf8d66054b713b2bcd
burstblacktuna <59651678+burstblacktuna@users.noreply.github.com>
2021-12-03 09:25:38 +0800

    a

--- src/homework/h8_1.java
+++ src/homework/h8_1.java
@@ -10,18 +10,18 @@ public class h8_1 {
                 {18.2, 17.3, 15.0, 13.4},
                 {23.8, 25.1, 20.6, 17.8},
                 {20.6, 21.5, 18.4, 15.7}};
-        String[] n = {"星期一", "星期二", "星期三", "星期四", "平均氣溫"};
-        String[] s = {"時段一", "時段二", "時段三"};
-        DecimalFormat df = new DecimalFormat("#.###");
+        String[] w = {"星期一", "星期二", "星期三", "星期四", "平均氣溫"};
+        String[] t = {"時段一", "時段二", "時段三"};
+        DecimalFormat df = new DecimalFormat("#.###");//控制小數點顯示位
         double sum;
         out.print("\t\t");
-        for (String i : n) {
+        for (String i : w) {
             out.print("\t" + i);
         }
         out.println();
         for (int i = 0; i < a.length; i++) {
             sum = 0.0;
-            out.print(s[i] + "\t\t");
+            out.print(t[i] + "\t\t");
             for (int j = 0; j < a[i].length; j++) {
                 sum += a[i][j];
                 out.print(a[i][j] + "\t");
@@ -49,13 +49,13 @@ public class h8_1 {
             for (int j = 0; j < a[i].length; j++) {
                 if (a[i][j] > maxtemp) {
                     maxtemp = a[i][j];
-                    maxday = n[j];
-                    maxtime = s[i];
+                    maxday = w[j];
+                    maxtime = t[i];
                 }
                 if (a[i][j] < mintemp) {
                     mintemp = a[i][j];
-                    minday = n[j];
-                    mintime = s[i];
+                    minday = w[j];
+                    mintime = t[i];
                 }
             }
         }
