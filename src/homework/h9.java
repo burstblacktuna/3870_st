6874de5e8d645b06b68dc659e17ab2f0cee713c5
burstblacktuna <a38708242@gmail.com>
2021-12-06 17:02:48 +0800

    h9

--- src/homework/h9.java
+++ src/homework/h9.java
@@ -1,10 +1,25 @@
 package homework;
 
+import java.util.Scanner;
 import static java.lang.System.*;
 
 public class h9 {
     public static void main(String[] args) {
-
-
+        var add = new Scanner(in);
+        for (int i =0;i<2;i++){
+            int n = add.nextInt();
+            addto(n);
+            out.println(even(n));
+        }
+    }
+    public static void addto(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        out.println(sum);
+    }
+    public static boolean even(int n) {
+        return (n % 2 == 0);
     }
 }
--- src/school/s5.java
+++ src/school/s5.java
@@ -1,9 +1,60 @@
 package school;
 
+import java.util.Scanner;
+
 import static java.lang.System.*;
 
 public class s5 {
     public static void main(String[] args) {
+        int[][][] z = {
+                {{3, 3, 3}, {3, 3, 3}},
+                {{3, 3, 3}, {3, 3, 3}},
+                {{3, 3, 3}, {3, 3, 3}}};
+        Scanner getIt = new Scanner(System.in);
+        System.out.println("Input 1 integer n: ");
+        int n = getIt.nextInt();
+        if (n > 0) {
+            final double[][][] M = new double[n][n][n];
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    for (int k = 0; k < n; k++)
+                        M[i][j][k] = 3.0;
+            System.out.println("Input 1 integer p: ");
+            int p = getIt.nextInt();
+            if (p > 0) {
+                for (int q = 1; q <= p; q++) {
+                    for (int i = 0; i < n; i++) {
+                        for (int j = 0; j < n; j++) {
+                            for (int k = 0; k < n; k++) {
+                                System.out.printf("%f ", Math.pow(matrixMult(M[i], M[i])[j][k], q));
+                            }
+                            System.out.println("\n");
+                        }
 
+                    }
+                    System.out.println("xxxxxxxxxxxxxxxxxxxxx");
+                }
+            } else {
+                System.out.println("Woops, you entered a negative p.");
+            }
+        } else {
+            System.out.println("Woops, you entered a negative n.");
+        }
+    }
+
+    public static double[][] matrixMult(double a[][], double b[][]) {
+        int aRows = a.length;
+        int aCols = a[0].length;
+        int bCols = b[0].length;
+        double[][] result = new double[aRows][bCols];
+        for (int i = 0; i < aRows; i++) {
+            for (int j = 0; j < bCols; j++) {
+                for (int k = 0; k < aCols; k++) {
+                    result[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+        return result;
     }
 }
+
