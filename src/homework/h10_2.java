d39af9fd017ad538f1c5521f163748b92ab3814e
burstblacktuna <59651678+burstblacktuna@users.noreply.github.com>
2021-12-19 18:58:44 +0800

    a

--- src/homework/h10_2.java
+++ src/homework/h10_2.java
@@ -1,23 +1,25 @@
 package homework;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 import static java.lang.System.*;
 
 public class h10_2 {
-    public static double circle(double r){
-        return (3.14*Math.pow(r,2));
+    public static double circle(double r) {
+        return (3.14 * Math.pow(r, 2));
     }
     /*------------------------------------------------------------*/
     public static void main(String[] args) {
-        var add = new Scanner(in);
-        int i =add.nextInt();
-        out.println("半徑="+i+",面積為:"+circle(i));
+        var point = new DecimalFormat("#.0#");
 
-        float f = add.nextFloat();
-        out.println("半徑="+f+",面積為:"+circle(f));
+        int i = 2;
+        out.println("半徑=" + i + ",面積為:" + circle(i));
 
-        double d = add.nextDouble();
-        out.println("半徑="+d+",面積為:"+circle(d));
+        float f = 2.3f;
+        out.println("半徑=" + f + ",面積為:" + point.format(circle(f)));
+
+        double d = 2.3;
+        out.println("半徑=" + d + ",面積為:" + point.format(circle(d)));
     }
 }
---
+++ src/t6.java
@@ -0,0 +1,10 @@
+import static java.lang.System.*;
+
+public class t6 {
+    public static void main(String[] args) {
+        float f = 2.3f;
+        out.print(f*f*8);
+
+
+    }
+}
