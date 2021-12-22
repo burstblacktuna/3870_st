0b5aa549dda63a7ca5984719e4e5c5367693a82f
burstblacktuna <59651678+burstblacktuna@users.noreply.github.com>
2021-12-22 15:05:21 +0800

    a

--- src/school/s08_t.java
+++ src/school/s08_t.java
@@ -3,17 +3,22 @@ package school;
 import static java.lang.System.*;
 
 public class s08_t {
-    double a = 0.0;
-    double b = 0.0;
-    double c = 0.0;
+
     public static void main(String[] args) {
-        s08_t t;
-        t= new s08_t();
+        CTriangle t;
+        t= new CTriangle();
         t.a=30.0;
         t.b=40.0;
         t.c=50.0;
         out.println(t.p());
     }
+
+}
+class CTriangle {
+    double a = 0.0;
+    double b = 0.0;
+    double c = 0.0;
+
     double p(){
         double s = (a+b+c)/2;
         return Math.sqrt(s*(s-a)*(s-b)*(s-c));
