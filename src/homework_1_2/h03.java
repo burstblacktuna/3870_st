f6521ce051efaab76e0bbcd5c5c2e75ba9ab1348
3870 <a38708242@gmail.com>
2022-03-15 16:57:56 +0800

    a

--- src/homework_1_2/h03.java
+++ src/homework_1_2/h03.java
@@ -1,17 +1,37 @@
 package homework_1_2;
 
-import java.text.DecimalFormat;
+import java.awt.*;
+import java.awt.event.*;
 
-import static java.lang.System.out;
+public class h03 extends Frame implements ActionListener {
+    static h03 frm = new h03();
+    static Button btn = new Button("Chick me");
+    static int c = 1;
 
-public class h03 {
     public static void main(String[] args) {
+        btn.addActionListener(frm);
+        frm.setLayout(new FlowLayout());
+        frm.setTitle("background=white");
+        frm.setSize(300, 300);
+        frm.add(btn);
+        frm.setVisible(true);
 
+        frm.addWindowListener(new WindowAdapter() {//關閉視窗
+            public void windowClosing(WindowEvent event) {
+                System.exit(0);
+            }});
+    }
 
-
-
-
-
-
+    public void actionPerformed(ActionEvent e) {
+        if (c == 0) {
+            frm.setBackground(Color.white);
+            frm.setTitle("background=white");
+            c = 1;
+        }
+        else {
+            frm.setBackground(Color.yellow);
+            frm.setTitle("background=yellow");
+            c = 0;
+        }
     }
 }
