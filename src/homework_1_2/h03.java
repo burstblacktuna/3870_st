5ec116e0a4b80e5a76b55c8f4ffda4802e760f28
3870 <a38708242@gmail.com>
2022-03-15 14:59:57 +0800

    a

---
+++ src/homework_1_2/h03.java
@@ -0,0 +1,17 @@
+package homework_1_2;
+
+import java.text.DecimalFormat;
+
+import static java.lang.System.out;
+
+public class h03 {
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+    }
+}
